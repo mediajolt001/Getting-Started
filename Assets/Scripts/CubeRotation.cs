c72970b1478c48460c38be5b9b894c01c168d4ab
Joseph Jolton <j@jolton.com>
2018-09-04 13:22:52 -0500

    Initial commit

---
+++ Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    // this is where i declare a variable
+    public float rotateSpeed;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        transform
+
+
+    }
+}
